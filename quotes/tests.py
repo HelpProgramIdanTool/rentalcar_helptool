13e84e181bc271df8e7ea2cb9f7e72da86b465b0
idancaliber <idancaliber@users.noreply.github.com>
2026-08-31 21:39:47 +0200

    Lock inquiry message panel during form scrolling

--- quotes/tests.py
+++ quotes/tests.py
@@ -72,6 +72,7 @@ class FirstInquiryTests(TestCase):
         self.assertContains(response, "Малые автомобили")
         self.assertContains(response, 'class="message-panel"')
         self.assertContains(response, 'class="form-scroll"')
+        self.assertContains(response, "body { margin:0;overflow:hidden; }")
         self.assertContains(response, 'autocomplete="off"')
         self.assertNotContains(response, "restoreDraft();")
 
